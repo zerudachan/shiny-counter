7210b1dcabd704ab76b42128417f517d9831a8ba
Ghi <zerudachan244@gmail.com>
2024-04-21 11:41:23 -0400

    Got shiny button now resets counter to 0

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
-}
+    count += 1;
+    countEl.textContent = count;
+};
 
 function decrease() {
     if (count > 0){
-        count -= 1
-        countEl.textContent = count
-    }
-}
+        count -= 1;
+        countEl.textContent = count;
+    };
+};
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-}
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    count = 0;
+};
